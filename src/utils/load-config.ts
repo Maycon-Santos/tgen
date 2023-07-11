b78911be5c6fd59db96b826193964f47a0dfd3e0
Maycon-Santos <mayconsantos.dev@gmail.com>
2023-07-11 09:15:13 -0300

    feat: add load user config util

---
+++ src/utils/load-config.ts
@@ -0,0 +1,30 @@
+import path = require('path')
+import fs = require('fs')
+import { CaseStyle } from './case-style'
+
+type Config = {
+  commands: {
+    [k: string]: {
+      dir: string
+      caseStyle?: CaseStyle
+      template: string
+      replaceInFile: {
+        regexp?: boolean
+        caseStyle?: CaseStyle
+        from: string
+        to: string
+      }[]
+    }
+  }
+}
+
+const loadConfig = (): Config => {
+  try {
+    const configRaw = fs.readFileSync(path.resolve(`./.tgen/.config`))
+    return JSON.parse(configRaw.toString())
+  } catch {
+    throw new Error('Could not load .config')
+  }
+}
+
+export default loadConfig
